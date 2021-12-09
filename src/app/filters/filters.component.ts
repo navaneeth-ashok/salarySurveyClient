f590f5f19f4f358545bf7b46c74d6da60cfc5f19
navaneeth_ashok_1 <navaneeth619@gmail.com>
2021-12-09 18:41:12 -0500

    Added filters, css, and mobile view

--- src/app/filters/filters.component.ts
+++ src/app/filters/filters.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-filters',
   templateUrl: './filters.component.html',
-  styleUrls: ['./filters.component.css']
+  styleUrls: ['./filters.component.css'],
 })
 export class FiltersComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
